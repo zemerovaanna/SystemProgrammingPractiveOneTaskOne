0b7a42e0c9c6052e049d255d8731b561c424ef8f
cvv_code <cvv_code@gmail.com>
2024-01-20 13:19:28 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppSum.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int number = 2;
+	std::cout << "Enter number: ";
+	std::cin >> number;
+
+	while (number < 1)
+	{
+		std::cout << "The number cannot be negative, please enter again: ";
+		std::cin >> number;
+	}
+
+	int sum = 0;
+	for (int i = 1; i <= number; i++) {
+		sum = sum + i;
+	}
+	std::cout << "The sum of all numbers from 1 to " << number << " = " << sum;
+}
